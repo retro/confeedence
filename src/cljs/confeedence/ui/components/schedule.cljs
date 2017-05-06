8bfb461b3437ff0284c8dc4120f5b8a1d12afe0a
Tibor Kranjcec <tibor@floatingpoint.io>
2017-05-06 19:17:02 +0200

    Start working on timeline

--- src/cljs/confeedence/ui/components/schedule.cljs
+++ src/cljs/confeedence/ui/components/schedule.cljs
@@ -3,20 +3,25 @@
             [keechma.toolbox.ui :refer [sub> route>]]
             [keechma.toolbox.css.core :refer-macros [defelement]]
             [keechma.toolbox.forms.helpers :as forms-helpers]
+            [cljsjs.moment]
             [confeedence.stylesheets.colors :refer [theme-colors-by-slug]]))
+
+(defn format-date [date]
+  (.format (js/moment date) "ll"))
 
 (defelement -action-link
   :tag :a
   :class [:bg-blue :c-white :px1 :py0-5 :rounded :text-decoration-none :inline-block])
 
 (defelement main-wrap
-  :class [:relative :flex :flex-wrap :flex-column :flex-auto])
+  :class [:relative :flex :flex-column :flex-auto]
+  :style [{:overflow-y "auto"}])
 
 (defelement center-div
   :class [:center])
 
 (defelement conference-info-wrap
-  :class [:flex :flex-column])
+  :class [])
 
 (defelement title-center
   :tag :h1
@@ -37,6 +42,48 @@
 (defelement talks-column-wrap
   :class [:flex :justify-center])
 
+(defelement date-circle
+  :tag :span
+  :class [:absolute :left-0 :right-0 :mx-auto :circle :flex :items-center :justify-center :c-white :bg-blue :c-button :center]
+  :style [{:width "7rem"
+           :height "7rem"}])
+
+(defelement timeline-item-wrap-left
+  :class [:bg-lightest-gray :left :p2 :relative]
+  :style [{:border-radius "0.2rem"
+           :width "38%"
+           :min-height "10rem"
+           :margin-left "3%"
+           :margin-right "9%"}
+          [:&:after {:content "''"
+                     :display "block"
+                     :position "absolute"
+                     :right "-9px"
+                     :top "25px"
+                     :width 0
+                     :height 0
+                     :border-style "solid"
+                     :border-width "10px 0 10px 10px"
+                     :border-color "transparent transparent transparent #ffffff"}]])
+
+(defelement timeline-item-wrap-right
+  :class [:bg-lightest-gray :right :p2 :relative]
+  :style [{:border-radius "0.2rem"
+           :width "38%"
+           :min-height "10rem"
+           :margin-left "9%"
+           :margin-right "3%"}
+          [:&:before {:content "''"
+                     :display "block"
+                     :position "absolute"
+                     :left "-10px"
+                     :top "25px"
+                     :width 0
+                     :height 0
+                     :border-style "solid"
+                     :border-width "10px 10px 10px 0"
+                     :border-color "transparent #ffffff transparent transparent"}]])
+
 (defn get-tag [conference tag]
   (get-in conference [:confeedence-tags tag]))
 
@@ -46,15 +93,33 @@
 
 (defn render-events [ctx events] 
   (let [current-route (route> ctx)]
-    [:ul
-     (doall (map (fn [e]
-                   [:li {:key (:id e)}
-                    (:name e)
-                    [:br]
-                    (:description e)
-                    [:a
-                     {:href (ui/url ctx (assoc current-route :form {:type "event" :id (:id e)}))}
-                     "Edit Event"]]) events))]))
+    [:ul.max-width-4.mx-auto.clearfix.list-reset.relative
+     (doall (map-indexed (fn [i,e]
+                           (if (= 0 (mod i 2))
+                             [:li.clearfix {:key (:id e)}
+                              [timeline-item-wrap-left
+                               (:name e)
+                               [:br]
+                               [:p (:description e)]
+                               [:br]
+                               [:a
+                               {:href (ui/url ctx (assoc current-route :form {:type "event" :id (:id e)}))}
+                               "Edit Event"]]
+                              [date-circle
+                               (format-date (get-in e [:when :startDate]))]
+                              ]
+                             [:li.clearfix {:key (:id e)}
+                              [timeline-item-wrap-right
+                               (:name e)
+                               [:br]
+                               [:p (:description e)]
+                               [:br]
+                               [:a
+                               {:href (ui/url ctx (assoc current-route :form {:type "event" :id (:id e)}))}
+                               "Edit Event"]]
+                              [date-circle
+                               (format-date (get-in e [:when :startDate]))]
+                              ])) events))]))
 
 (defn render [ctx]
   (let [current-route (route> ctx)
