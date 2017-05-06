11268b9e8e449122830d228b4780a0486cefb0f6
Tibor Kranjcec <tibor@floatingpoint.io>
2017-05-06 14:28:49 +0200

    Add additional typo class (c-link)

--- src/cljs/confeedence/stylesheets/typography.cljs
+++ src/cljs/confeedence/stylesheets/typography.cljs
@@ -2,24 +2,28 @@
   (:require [confeedence.stylesheets.colors :refer [colors]]))
 
 (defn stylesheet []
-  [:.c-title {:font-family "'Open Sans', sans-serif"
-              :font-weight 800
-              :font-size "2rem"}
-   :.c-subtitle {:font-family "'Open Sans', sans-serif"
-                 :font-weight 700
-                 :font-size "1.5rem"}
-   :.c-emphasized {:font-family "'Open Sans', sans-serif"
-                   :font-weight 600
-                   :font-size "1.25rem"}
-   :.c-body {:font-family "'Raleway', sans-serif"
-             :font-weight 400
-             :font-size "1rem"}
-   :h1 {:font-family "'Open Sans', sans-serif"
-        :font-weight 800
-        :font-size "2rem"}
-   :h2 {:font-family "'Open Sans', sans-serif"
-                 :font-weight 700
-                 :font-size "1.5rem"}
-   :h3 {:font-family "'Open Sans', sans-serif"
-        :font-weight 600
-        :font-size "1.25rem"}])
+  [[:.c-title {:font-family "'Open Sans', sans-serif"
+               :font-weight 800
+               :font-size "3rem"}]
+   [:.c-subtitle {:font-family "'Open Sans', sans-serif"
+                  :font-weight 700
+                  :font-size "1.5rem"}]
+   [:.c-emphasized {:font-family "'Open Sans', sans-serif"
+                    :font-weight 600
+                    :font-size "1.25rem"}]
+   [:.c-body {:font-family "'Raleway', sans-serif"
+              :font-weight 400
+              :font-size "1rem"}]
+   [:.c-link {:font-family "'Raleway', sans-serif"
+              :font-weight 200
+              :font-size "3rem"}]
+   [:h1 {:font-family "'Open Sans', sans-serif"
+         :font-weight 800
+         :font-size "2rem"}]
+   [:h2 {:font-family "'Open Sans', sans-serif"
+         :font-weight 700
+         :font-size "1.5rem"}]
+   [:h3 {:font-family "'Open Sans', sans-serif"
+         :font-weight 600
+         :font-size "1.25rem"}]
+   [:.uppercase {:text-transform "uppercase"}]])
