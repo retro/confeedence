67d9efdda8add45c2a513637b3df6b68296ca961
Tibor Kranjcec <tibor@floatingpoint.io>
2017-05-06 20:58:02 +0200

    Style 'Edit' Page

--- src/cljs/confeedence/ui/components/schedule_list.cljs
+++ src/cljs/confeedence/ui/components/schedule_list.cljs
@@ -1,21 +1,46 @@
 (ns confeedence.ui.components.schedule-list
   (:require [keechma.ui-component :as ui]
+            [keechma.toolbox.css.core :refer-macros [defelement]]
             [keechma.toolbox.ui :refer (sub>)]))
+
+(defelement conference-wrap
+  :class [:border-right :bd-lighter-gray]
+  :style [])
+
+(defelement title
+  :tag :h3
+  :class [:c-white :bg-green :m0 :px1 :py2])
+
+(defelement conference-list-wrap
+  :tag :ul
+  :class [:list-reset :m0])
+
+(defelement conference-list-item
+  :tag :li
+  :class [:border-top :bd-lighter-gray :bg-h-lighter-gray :flex])
+
+(defelement link
+  :tag :a
+  :class [:text-decoration-none :c-body :c-dark :p1 :flex-auto :pointer])
+
+(defelement no-conferences
+  :class [:bg-dark :c-white :p2 :c-body])
 
 (defn render [ctx]
   (let [schedules (sub> ctx :schedules)
         loading? (= :pending (:status (sub> ctx :schedules-meta)))]
     (when-not loading?
       (if (seq schedules)
-        [:div
-         [:div "Created Schedules"]
-         [:ul
-          (doall (map (fn [s]
-                        [:li {:key (:id s)}
-                         [:a {:href (ui/url ctx {:page "edit" :id (:id s)})}
-                          (:name s)]])
-                      schedules))]]
-        [:div.bg-blue.c-white.p1 "There are no created schedules"]))))
+        [conference-wrap
+         [title "Created Conferences"]
+         [conference-list-wrap
+          (doall (map-indexed (fn [i,s]
+                                [conference-list-item {:key (:id s)
+                                                       :class (when (= (inc i) (count schedules)) "border-bottom")}
+                                 [link {:href (ui/url ctx {:page "edit" :id (:id s)})}
+                                  (:name s)]])
+                              schedules))]]
+        [no-conferences "There are no created schedules"]))))
 
 (def component
   (ui/constructor {:renderer render
