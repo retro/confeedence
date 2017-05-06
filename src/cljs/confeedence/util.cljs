1d25020301f4db9f7784f73cdde4ba09769da070
Tibor Kranjcec <tibor@floatingpoint.io>
2017-05-06 16:09:57 +0200

    Add format date function

--- src/cljs/confeedence/util.cljs
+++ src/cljs/confeedence/util.cljs
@@ -1,4 +1,10 @@
-(ns confeedence.util)
+(ns confeedence.util
+  (:require [clojure.string :as str]))
 
 (defn get-access-token [app-db]
   (get-in app-db [:kv :access-token]))
+
+(defn format-date [datetime]
+  (let [date (first (str/split datetime "T"))
+        [y m d] (str/split date "-")]
+    (str m "/" d "/" y)))
